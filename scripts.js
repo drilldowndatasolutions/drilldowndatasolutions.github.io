aa17a432b4685f74f5529690bd8953dc541ff0ee
Dylan Burd <burddylan@gmail.com>
2023-04-30 22:38:17 -0400

    c4

--- scripts.js
+++ scripts.js
@@ -2,10 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const navToggle = document.querySelector('.nav-toggle');
   const navMenu = document.querySelector('nav ul');
 
-  navToggle.addEventListener('click', function () {
-    navMenu.classList.toggle('show');
-  });
+  navToggle.addEventListener('click', toggleNav);
 });
+
+function toggleNav() {
+  const navMenu = document.querySelector('nav ul');
+  navMenu.classList.toggle('show');
+}
 
 const backToTopButton = document.getElementById("back-to-top");
 
@@ -20,29 +23,3 @@ window.addEventListener("scroll", () => {
 backToTopButton.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
-
-document.getElementById("back-to-top").addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
-
-window.addEventListener("scroll", function () {
-  const backToTopButton = document.getElementById("back-to-top");
-  if (window.pageYOffset > 300) {
-    backToTopButton.style.display = "block";
-  } else {
-    backToTopButton.style.display = "none";
-  }
-});
-
-function smoothScrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-}
-
-const backToTopButton = document.getElementById("back-to-top");
-backToTopButton.addEventListener("click", smoothScrollToTop);
