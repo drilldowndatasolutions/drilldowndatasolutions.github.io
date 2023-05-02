d8d0191e2c1011651d82a253a837ea77a6cb202b
Dylan Burd <burddylan@gmail.com>
2023-05-02 15:14:38 -0400

    contact updated

--- scripts.js
+++ scripts.js
@@ -22,3 +22,32 @@ document.getElementById("back-to-top").addEventListener("click", function () {
     behavior: "smooth"
   });
 });
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const xhr = new XMLHttpRequest();
+  const formData = new FormData(event.target);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200 && xhr.responseText === "success") {
+        showMessagePopup();
+      } else {
+        alert("An error occurred while sending the email. Please try again.");
+      }
+    }
+  };
+
+  xhr.open("POST", "send_email.php", true);
+  xhr.send(formData);
+});
+
+function showMessagePopup() {
+  const popup = document.getElementById("message-popup");
+  popup.style.display = "block";
+
+  setTimeout(function () {
+    popup.style.display = "none";
+  }, 5000);
+}
